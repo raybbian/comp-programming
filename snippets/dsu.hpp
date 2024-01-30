d801af87c048582e81a24dae05309b90ef5eebce
Raymond Bian <raybbian@gmail.com>
2024-01-30 17:47:03 -0500

    formatted files

--- snippets/dsu.hpp
+++ snippets/dsu.hpp
@@ -1,25 +1,21 @@
 struct DSU {
-	vector<int> e;
-	DSU(int n) { e = vector<int>(n, -1); }
-	
-	int get(int x) {
-		return e[x] < 0 ? x : e[x] = get(e[x]); 
-	}
+  vector<int> e;
+  DSU(int n) { e = vector<int>(n, -1); }
 
-	bool is_same(int a, int b) {
-		return get(a) == get(b);
-	}
+  int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
 
-	int size(int x) {
-		return -e[get(x)];
-	}
+  bool is_same(int a, int b) { return get(a) == get(b); }
 
-	bool unite(int a, int b) {
-		x = get(x), y = get(y);
-		if (x == y) return false;
-		if (e[x] > e[y]) swap(x, y);
-		e[x] += e[y];
-		e[y] = x;
-		return true;
-	}
+  int size(int x) { return -e[get(x)]; }
+
+  bool unite(int a, int b) {
+    x = get(x), y = get(y);
+    if (x == y)
+      return false;
+    if (e[x] > e[y])
+      swap(x, y);
+    e[x] += e[y];
+    e[y] = x;
+    return true;
+  }
 };
--- snippets/sieve.hpp
+++ snippets/sieve.hpp
@@ -1,12 +1,12 @@
 vector<bool> is_prime;
 void sieve(int n) {
-	is_prime = vector<bool>(n + 1, true);
-	is_prime[0] = is_prime[1] = false;
-	for(int i = 2; i * i <= n; i++) {
-		if (is_prime[i]) {
-			for(int j = i * i; j <= n; j += i) {
-				is_prime[j] = false;
-			}
-		}
-	}
+  is_prime = vector<bool>(n + 1, true);
+  is_prime[0] = is_prime[1] = false;
+  for (int i = 2; i * i <= n; i++) {
+    if (is_prime[i]) {
+      for (int j = i * i; j <= n; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
 }
