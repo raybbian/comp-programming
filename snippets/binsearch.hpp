1bdfa0ae25ced238671d009f6eea9b20a8b58631
Raymond Bian <raybbian@gmail.com>
2024-01-30 17:42:56 -0500

    refactored into modular snippets

---
+++ snippets/binsearch.hpp
@@ -0,0 +1,19 @@
+// binary search
+template <typename T, typename U> T last_true(T l, T r, U f) {
+  l--;
+  assert(l <= r);
+  while (l < r) {
+    T mid = l + (r - l + 1) / 2;
+    f(mid) ? l = mid : r = mid - 1;
+  }
+  return l;
+}
+template <typename T, typename U> T first_true(T l, T r, U f) {
+  r++;
+  assert(l <= r);
+  while (l < r) {
+    T mid = l + (r - l) / 2;
+    f(mid) ? r = mid : l = mid + 1;
+  }
+  return l;
+}
---
+++ snippets/pbds.hpp
@@ -0,0 +1,12 @@
+// policy based ds
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T>
+using ordered_multiset =
+    tree<T, null_type, less_equal<T>, rb_tree_tag,
+         tree_order_statistics_node_update>; // lower_bound and upper_bound
+                                             // reversed!
