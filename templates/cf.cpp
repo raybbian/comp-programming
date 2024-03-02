a56c639f99635dd17d0255a1cea14d60ad58d2aa
Raymond Bian <raybbian@gmail.com>
2024-03-02 12:18:44 -0500

    trick where sorting best preorder can compare them, simple D&C

--- templates/cf.cpp
+++ templates/cf.cpp
@@ -14,9 +14,12 @@ using pdd = pair<db, db>;
 
 // loops
 #define REP2(i, n) for (int i = 0; i < (n); i++)
-#define REP3(i, a, b) for (int i = (a); (a) <= (b) ? i <= (b) : i >= (a); (a) <= (b) ? i++ : i--)
+#define REP3(i, a, b) for (int i = (a); i <= (b); i++)
+#define PER2(i, n) for (int i = (n)-1; i >= 0; i--)
+#define PER3(i, a, b) for (int i = (a); i >= (b); i--)
 #define GET_MACRO(a, b, c, d, ...) d
 #define rep(...) GET_MACRO(__VA_ARGS__, REP3, REP2)(__VA_ARGS__)
+#define per(...) GET_MACRO(__VA_ARGS__, PER3, PER2)(__VA_ARGS__)
 #define each(i, a) for (auto &i : (a))
 
 // n-dimensional vector
