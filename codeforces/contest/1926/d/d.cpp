fcff782d4d59834c7613c159934288dec2c9091b
Raymond Bian <raybbian@gmail.com>
2024-03-04 15:04:46 -0500

    started using cli for test and submission

---
+++ codeforces/contest/1926/d/d.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typing is hard
+using ll = long long;
+using db = long double;
+using str = string;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<db, db>;
+#define fi first
+#define se second
+#define ln "\n"
+
+// loops
+#define REP2(i, n) for (int i = 0; i < (n); i++)
+#define REP3(i, a, b) for (int i = (a); i <= (b); i++)
+#define PER2(i, n) for (int i = (n)-1; i >= 0; i--)
+#define PER3(i, a, b) for (int i = (a); i >= (b); i--)
+#define GET_MACRO(a, b, c, d, ...) d
+#define rep(...) GET_MACRO(__VA_ARGS__, REP3, REP2)(__VA_ARGS__)
+#define per(...) GET_MACRO(__VA_ARGS__, PER3, PER2)(__VA_ARGS__)
+#define each(i, a) for (auto &i : (a))
+
+// n-dimensional vector
+template <typename T> vector<T> ndvec(size_t size, T initial_value) {
+    return vector<T>(size, initial_value);
+}
+template <typename T, typename... U> auto ndvec(size_t head, U &&...u) {
+    auto inner = ndvec<T>(u...);
+    return vector<decltype(inner)>(head, inner);
+}
+
+// bitwise ops
+constexpr int popcnt(int x) { return __builtin_popcount(x); }            // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); } // floor(log2(x))
+
+// better math
+ll c_div(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); }
+ll f_div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+
+// check and assign max and min
+template <typename T> bool chk_min(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <typename T> bool chk_max(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+#ifdef LOCAL
+#include "/home/rayb/Documents/comp-programming/debug/debug.hpp"
+#define dbg(...) Debug(#__VA_ARGS__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+// insert snippets below:
+// custom hash
+struct chash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    template <typename T, typename U> size_t operator()(const pair<T, U> &x) const {
+        uint64_t a = (*this)(x.fi);
+        uint64_t b = (*this)(x.se);
+        return a + 3 * b;
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    unordered_map<int, int, chash> cnt;
+    int ans = n;
+    rep(i, n) {
+        int a;
+        cin >> a;
+        int conj = ~a & ~(1 << 31);
+        dbg(a, conj);
+        if (cnt.count(conj)) {
+            cnt[conj]--;
+            if (cnt[conj] == 0)
+                cnt.erase(conj);
+            ans--;
+        } else {
+            cnt[a]++;
+        }
+    }
+    cout << ans << ln;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
+
+/*
+ * SANITY CHECKLIST:
+ * 1. re-read the problem
+ * 2. consider edge cases (n=1, overflow)
+ * 3. come up with counter cases
+ * 4. move on to the next problem
+ * 5. re-read the problem
+ * 6. re-read the problem upside-down
+ * 7. re-read the problem backwords
+ * 8. fucking re-read the problem
+ */
