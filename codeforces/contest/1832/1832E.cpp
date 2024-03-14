a8a7b78a02516c778f09225aef55e2992ae77da7
Raymond Bian <raybbian@gmail.com>
2024-03-13 22:46:03 -0400

    dp + hockeystick

---
+++ codeforces/contest/1832/1832E.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typing is hard
+using ll = long long;
+using db = long double;
+using str = string;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<db, db>;
+#define fi first
+#define se second
+#define ln "\n"
+
+// loops
+#define REP2(i, n) for (int i = 0; i < (n); i++)
+#define REP3(i, a, b) for (int i = (a); i <= (b); i++)
+#define PER2(i, n) for (int i = (n)-1; i >= 0; i--)
+#define PER3(i, a, b) for (int i = (a); i >= (b); i--)
+#define GET_MACRO(a, b, c, d, ...) d
+#define rep(...) GET_MACRO(__VA_ARGS__, REP3, REP2)(__VA_ARGS__)
+#define per(...) GET_MACRO(__VA_ARGS__, PER3, PER2)(__VA_ARGS__)
+#define each(i, a) for (auto &i : (a))
+
+// n-dimensional vector
+template <typename T> vector<T> ndvec(size_t size, T initial_value) {
+    return vector<T>(size, initial_value);
+}
+template <typename T, typename... U> auto ndvec(size_t head, U &&...u) {
+    auto inner = ndvec<T>(u...);
+    return vector<decltype(inner)>(head, inner);
+}
+
+// bitwise ops
+constexpr int popcnt(int x) { return __builtin_popcount(x); }            // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); } // floor(log2(x))
+
+// better math
+ll c_div(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); }
+ll f_div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+
+// check and assign max and min
+template <typename T> bool chk_min(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <typename T> bool chk_max(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+#ifdef LOCAL
+#include "/home/rayb/Documents/comp-programming/debug/debug.hpp"
+#define dbg(...) Debug(#__VA_ARGS__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+// insert snippets below:
+template <typename T> constexpr T power(T a, ll b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2)
+            res *= a;
+    }
+    return res;
+}
+
+template <int P> struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(ll x) : x{norm(x % P)} {}
+
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += P;
+        }
+        if (x >= P) {
+            x -= P;
+        }
+        return x;
+    }
+    constexpr int val() const { return x; }
+    explicit constexpr operator int() const { return x; }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(P - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, P - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) {
+        x = 1LL * x * rhs.x % P;
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) { return *this *= rhs.inv(); }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        ll v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) { return lhs.val() == rhs.val(); }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) { return lhs.val() != rhs.val(); }
+};
+
+using modint107 = MInt<1000000007>;
+using modint998 = MInt<998244353>;
+
+using mint = modint998;
+
+void solve() {
+    ll n, a1, x, y, m, k;
+    cin >> n >> a1 >> x >> y >> m >> k;
+    vector<ll> a(n + 1), p(n + 1);
+    a[1] = a1;
+    p[1] = a1;
+    rep(i, 2, n) {
+        a[i] = (a[i - 1] * x + y) % m;
+        p[i] = p[i - 1] + a[i];
+    }
+
+    dbg(a, p);
+
+    auto dp = ndvec<mint>(n + 1, k + 1, (mint)0);
+    auto pref = ndvec<mint>(n + 1, k + 1, (mint)0);
+
+    rep(i, 1, n) {
+        dp[i][1] = dp[i - 1][1] + p[i];
+        pref[i][1] = pref[i - 1][1] + dp[i][1];
+    }
+
+    dbg(dp, pref);
+
+    rep(j, 2, k) {
+        rep(i, 1, n) {
+            if (i - j >= 0) {
+                dp[i][j] = pref[i - 1][j - 1];
+                pref[i][j] = pref[i - 1][j] + dp[i][j];
+            }
+        }
+    }
+
+    dbg(dp, pref);
+    ll ans = dp[1][k].val();
+    rep(i, 2, n) { ans ^= (ll)dp[i][k].val() * (ll)i; }
+    cout << ans << "\n";
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+}
+
+/*
+ * SANITY CHECKLIST:
+ * 1. re-read the problem
+ * 2. consider edge cases (n=1, overflow)
+ * 3. come up with counter cases
+ * 4. move on to the next problem
+ * 5. re-read the problem
+ * 6. re-read the problem upside-down
+ * 7. re-read the problem backwords
+ * 8. fucking re-read the problem
+ */
