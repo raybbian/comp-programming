86d73a708c93287243de76ba3c226ba0c38afcbb
Raymond Bian <raybbian@gmail.com>
2024-03-21 21:10:48 -0400

    nice quicksort/mergesort interactive problem

---
+++ codeforces/contest/1918/1918E.cpp
@@ -0,0 +1,196 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typing is hard
+using ll = long long;
+using db = long double;
+using str = string;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<db, db>;
+#define fi first
+#define se second
+#define ln "\n"
+
+// loops
+#define REP2(i, n) for (int i = 0; i < (n); i++)
+#define REP3(i, a, b) for (int i = (a); i <= (b); i++)
+#define PER2(i, n) for (int i = (n)-1; i >= 0; i--)
+#define PER3(i, a, b) for (int i = (a); i >= (b); i--)
+#define GET_MACRO(a, b, c, d, ...) d
+#define rep(...) GET_MACRO(__VA_ARGS__, REP3, REP2)(__VA_ARGS__)
+#define per(...) GET_MACRO(__VA_ARGS__, PER3, PER2)(__VA_ARGS__)
+#define each(i, a) for (auto &i : (a))
+
+// n-dimensional vector
+template <typename T> vector<T> ndvec(size_t size, T initial_value) {
+    return vector<T>(size, initial_value);
+}
+template <typename T, typename... U> auto ndvec(size_t head, U &&...u) {
+    auto inner = ndvec<T>(u...);
+    return vector<decltype(inner)>(head, inner);
+}
+
+// bitwise ops
+constexpr int popcnt(int x) { return __builtin_popcount(x); }            // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); } // floor(log2(x))
+
+// better math
+ll c_div(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); }
+ll f_div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+
+// check and assign max and min
+template <typename T> bool chk_min(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <typename T> bool chk_max(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+#ifdef LOCAL
+#include "/home/rayb/Documents/comp-programming/debug/debug.hpp"
+#define dbg(...) Debug(#__VA_ARGS__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+// insert snippets below:
+
+int ask(int ind) {
+    cout << "? " << ind << ln;
+    cout.flush();
+    char resp;
+    cin >> resp;
+    if (resp != '<' && resp != '>' && resp != '=')
+        assert(false);
+    return resp - 61;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int x = 0;
+    int hi = INT_MIN, lo = INT_MAX;
+    int hi_ind = -1, lo_ind = -1;
+    rep(i, 1, n) {
+        while (true) {
+            int resp = ask(i);
+            dbg(resp);
+            if (resp == 1) {
+                x++;
+            } else if (resp == -1) {
+                x--;
+                break;
+            } else {
+                break;
+            }
+        }
+        if (x > hi) {
+            hi = x;
+            hi_ind = i;
+        }
+    }
+    x = 0;
+    rep(i, 1, n) {
+        while (true) {
+            int resp = ask(i);
+            if (resp == 1) {
+                x++;
+                break;
+            } else if (resp == -1) {
+                x--;
+            } else {
+                break;
+            }
+        }
+        if (x < lo) {
+            lo = x;
+            lo_ind = i;
+        }
+    }
+
+    dbg(lo_ind, hi_ind);
+    unordered_map<int, vector<int>> check;
+    vector<array<int, 2>> bounds(n + 1);
+    rep(i, 1, n) bounds[i][0] = 1, bounds[i][1] = n;
+
+    vector<int> resolved(n + 1, -1);
+    resolved[lo_ind] = 1;
+    resolved[hi_ind] = n;
+    // set x = 1;
+    rep(j, n) ask(lo_ind);
+
+    rep(i, bits(n) / 2 + 1) {
+        rep(j, 1, n) {
+            if (resolved[j] != -1)
+                continue;
+            int mid = (bounds[j][0] + bounds[j][1]) / 2;
+            check[mid].push_back(j);
+        }
+        rep(x, 1, n) {
+            while (check.count(x) && !check[x].empty()) {
+                int ind = check[x].back();
+                check[x].pop_back();
+
+                int resp = ask(ind);
+                if (resp == -1) {
+                    bounds[ind][1] = x - 1;
+                    if (bounds[ind][0] == bounds[ind][1])
+                        resolved[ind] = bounds[ind][0];
+                    ask(hi_ind);
+                } else if (resp == 1) {
+                    bounds[ind][0] = x + 1;
+                    if (bounds[ind][0] == bounds[ind][1])
+                        resolved[ind] = bounds[ind][0];
+                    ask(lo_ind);
+                } else {
+                    resolved[ind] = x;
+                }
+            }
+            ask(hi_ind);
+        }
+        rep(j, 1, n) {
+            if (resolved[j] != -1)
+                continue;
+            int mid = (bounds[j][0] + bounds[j][1]) / 2;
+            check[mid].push_back(j);
+        }
+        per(x, n, 1) {
+            while (check.count(x) && !check[x].empty()) {
+                int ind = check[x].back();
+                check[x].pop_back();
+
+                int resp = ask(ind);
+                if (resp == -1) {
+                    bounds[ind][1] = x - 1;
+                    ask(hi_ind);
+                } else if (resp == 1) {
+                    bounds[ind][0] = x + 1;
+                    ask(lo_ind);
+                } else {
+                    resolved[ind] = x;
+                }
+            }
+            ask(lo_ind);
+        }
+    }
+    cout << "! ";
+    rep(i, 1, n) { cout << resolved[i] << " \n"[i == n]; }
+    cout.flush();
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
+
+/*
+ * SANITY CHECKLIST:
+ * 1. re-read the problem
+ * 2. consider edge cases (n=1, overflow)
+ * 3. come up with counter cases
+ * 4. move on to the next problem
+ * 5. re-read the problem
+ * 6. re-read the problem upside-down
+ * 7. re-read the problem backwords
+ * 8. fucking re-read the problem
+ */
