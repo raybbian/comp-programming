1b2073cb86484676604c9b9181713e8518bea00c
Raymond Bian <raybbian@gmail.com>
2024-09-27 10:00:07 -0400

    Simple dfs and greedy

---
+++ codeforces/contest/767C.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// typing is hard
+using ll = long long;
+using db = long double;
+using str = string;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<db, db>;
+#define fi first
+#define se second
+#define ln "\n"
+
+// loops
+#define REP2(i, n) for (int i = 0; i < (n); i++)
+#define REP3(i, a, b) for (int i = (a); i <= (b); i++)
+#define PER2(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define PER3(i, a, b) for (int i = (a); i >= (b); i--)
+#define GET_MACRO(a, b, c, d, ...) d
+#define rep(...) GET_MACRO(__VA_ARGS__, REP3, REP2)(__VA_ARGS__)
+#define per(...) GET_MACRO(__VA_ARGS__, PER3, PER2)(__VA_ARGS__)
+#define each(i, a) for (auto &(i) : (a))
+
+// n-dimensional vector
+template <typename T> vector<T> ndvec(size_t size, T initial_value) {
+    return vector<T>(size, initial_value);
+}
+template <typename T, typename... U> auto ndvec(size_t head, U &&...u) {
+    auto inner = ndvec<T>(u...);
+    return vector<decltype(inner)>(head, inner);
+}
+
+// bitwise ops
+constexpr int popcnt(int x) { return __builtin_popcount(x); }            // # of bits set
+constexpr int bits(int x) { return x == 0 ? 0 : 31 - __builtin_clz(x); } // floor(log2(x))
+
+// better math
+ll c_div(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); }
+ll f_div(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); }
+
+// check and assign max and min
+template <typename T> bool chk_min(T &a, const T &b) { return b < a ? a = b, 1 : 0; }
+template <typename T> bool chk_max(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+#ifdef LOCAL
+#include "/home/rayb/comp-programming/debug/debug.hpp"
+#define dbg(...) Debug(#__VA_ARGS__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
+
+// insert snippets below:
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vector<int>> adj(n + 1);
+    vector<int> temp(n + 1);
+    int sum_t = 0;
+    rep(i, 1, n) {
+        int par;
+        cin >> par >> temp[i];
+        adj[par].push_back(i);
+        sum_t += temp[i];
+    }
+    if (sum_t % 3 != 0) {
+        cout << -1 << ln;
+        return;
+    }
+
+    vector<int> ans;
+    auto dfs = [&](int node, auto &&dfs) -> int {
+        int subtree_t = temp[node];
+        for (int next : adj[node]) {
+            subtree_t += dfs(next, dfs);
+        }
+        if (subtree_t == sum_t / 3) {
+            ans.push_back(node);
+            return 0;
+        }
+        return subtree_t;
+    };
+    dfs(adj[0][0], dfs);
+
+	dbg(ans);
+    if (ans.size() < 3) {
+        cout << -1 << ln;
+        return;
+    }
+	cout << ans[0] << ' ' << ans[1] << ln;
+}
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+}
+
+/*
+ * SANITY CHECKLIST:
+ * 1. re-read the problem
+ * 2. consider edge cases (n=1, overflow)
+ * 3. come up with counter cases
+ * 4. move on to the next problem
+ * 5. re-read the problem
+ * 6. re-read the problem upside-down
+ * 7. re-read the problem backwords
+ * 8. fucking re-read the problem
+ */
